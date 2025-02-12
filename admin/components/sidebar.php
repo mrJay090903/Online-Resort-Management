cb9c94f55812c50a8c324495e9a8cb5aef405141
MichaelJohnDacillo <dacillomikejohn436@gmail.com>
2025-02-12 13:17:49 +0800

    get

--- admin/components/sidebar.php
+++ admin/components/sidebar.php
@@ -127,15 +127,13 @@
           <span x-show="open" x-cloak class="ml-3">Guest Feedback</span>
         </a>
 
-        <a href="logout.php" class="flex items-center px-6 py-3 text-white hover:bg-emerald-600" :class="{'justify-center': !open}">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-          </svg>
-          <span x-show="open" x-cloak class="ml-3">Logout</span>
-        </a>        
-      </nav>
-    </div>
-  </div>
-</body>
+    <a href="logout.php" class="flex items-center px-6 py-3 text-white hover:bg-emerald-600">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24"
+        stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+      </svg>
+      Logout
+    </a>
+  </nav>
 </div>
